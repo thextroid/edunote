8fe6ac5929ce3fc322a2196ab716d4cc9136786e
Israel <thexfer@gmail.com>
2017-04-18 21:59:11 -0400

    version 2 de McClusky

--- McKluskey.cpp
+++ McKluskey.cpp
@@ -12,7 +12,9 @@ enum VISITED{ cancel,ok,pending };
 using namespace std;
 typedef vector<int> vi;
 typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
 typedef vector<ii> vii;
+typedef vector<iii> viii;
 int n;
 int countBit(int& x,int guion=0){
 	int i=-1,count=0;
@@ -21,7 +23,7 @@ int countBit(int& x,int guion=0){
 			count+= ( (x&(1<<i))>>i);//cuenta los bit encendidos (1's)
 	return count;
 }
-bool cmp(int a,int b){return countBit(a)<countBit(b);}
+bool cmp(ii a,ii b){return countBit(a.first)<countBit(b.second);}
 void toMask(int &a,int mask=0){
 	int i=n;
 	cout.fill('0');
@@ -40,59 +42,56 @@ int isAdjacent(int& a,int& b,int u=0,int v=0){
 	}
 	return adjacency==1?v:0;// la adyacencia debe ser de solo "un" bit(0 o 1)
 }
-vi v,guion,term;
+vii v,term,unchecked, rest;
 void McClusky(){
 	sort(v.begin(),v.end(),cmp);
 	int iter=1;
-	vii unchecked, rest;
 	while(1){
-		vi group[n+1], sign[n+1], vist[n+1];
-		rep(j,0,v.size() ){
-			group[countBit(v[j],guion[j])].pz(v[j]),
-			sign[countBit(v[j],guion[j])].pz(guion[j]),
-			vist[countBit(v[j],guion[j])].pz(cancel);
-		}
+		viii group[n+1];
+		for(vii::iterator i = v.begin();i!=v.end();i++)
+			group[countBit(i->first,i->second)].pz( iii( i->first, ii(i->second,cancel) ) );
 		v.clear();
 		term.assign(v.begin(),v.end());
-		guion.clear();
 		map<ii,int> mp;
 		rest.assign(unchecked.begin(),unchecked.end());
 		unchecked.clear();
 		int x,y,s1,s2,adjacent,reset=1,flag=1;
 		rep(g,0,n){
-			rep(i,0,group[g].size()){
-				x = group[g][i],	s1 = sign[g][i];
+			rep(i,0,group[g].size() ){
+				x = group[g][i].first,	s1 = group[g][i].second.first;
 				rep(j,0,group[g+1].size()){
-					y = group[g+1][j],	s2 = sign[g+1][j];
+					y = group[g+1][j].first,	s2 = group[g+1][j].second.first;
 					adjacent = isAdjacent(x,y,s1,s2);
 					if( adjacent ){
-						vist[g][i]= ok;vist[g+1][j]= ok;// dos parejas distintas que debe ser checkeadas
+						group[g][i].second.second = ok,
+						group[g+1][j].second.second = ok;// dos parejas distintas que debe ser checkeadas
 						if( mp[ii(x,adjacent)])	continue;//dos pares distintos, que generan un patron ya registrado
-						mp[ii(x,adjacent)]=1,v.pz(x),guion.pz(adjacent);
+						mp[ii(x,adjacent)]=1,v.pz(ii(x,adjacent));
 						reset=1;
 					}
 				}
-				if(vist[g][i]==cancel )	unchecked.pz(ii(x,s1));
+				if(group[g][i].second.second==cancel )	unchecked.pz(ii(x,s1));
 			}
 		}
 		if(!v.size())break;
 		printf("=========== ITERACION  %d ===========\n",(iter++));
-		rep(j,0,v.size()){toMask(v[j],guion[j]);printf("\n");}
+		rep(j,0,v.size()){toMask(v[j].first,v[j].second);printf("\n");}
 	}
 	printf("_______________________________\n");
-	rep(j,0,term.size()){toMask(term[j],guion[j]);printf("\n");}
+	sort(term.begin(),term.end());
+	rep(j,0,term.size()){toMask(term[j].first,term[j].second);printf("\n");}
 	rep(j,0,rest.size()){toMask(rest[j].first,rest[j].second);printf("\n");}
 }
-void manually(vi& v){
+void manually(vii& v){
 	int a;
-	while(scanf("%d",&a)!=EOF && a!=-1)	v.pz(a);
+	while(scanf("%d",&a)!=EOF && a!=-1)	v.pz(ii(a,0));
 }
-inline void shurfle(vi& v){
+inline void shurfle(vii& v){
 	int a=1<<n;
 	set<int> tmp;
 	rep(i,0,a)	tmp.insert(rand()%a);
 	for(set<int>::iterator it=tmp.begin();it!=tmp.end();it++)
-		v.pz(*it),	cout<<(*it)<<" ";
+		v.pz(ii(*it,0)),	cout<<(*it)<<" ";
 	cout<<endl;
 }
 int main(){
@@ -102,7 +101,7 @@ int main(){
 		if(n>20 || n==0) break;
 		// shurfle(v);
 		manually(v);
-		guion.resize(v.size(),0);
+		// guion.resize(v.size(),0);
 		McClusky();
 	}
 	return 0;
